ea1e34913a15ebc9156e98c687e3c0149f81cced
bsamu <bsamuel0220@gmail.com>
2022-05-15 19:14:14 +0200

    warning added with template, styles

--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-warning-alert',
-  templateUrl: './warning-alert.component.html',
+  template: `<p>This is the warning alert from template in .ts.</p>`,
   styles: [`
     p {
       color: red;
